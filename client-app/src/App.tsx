f620a01252a5340ed78c7b5cd8fd3a535733a0dc
florianfani <florian.fani@hotmail.com>
2022-04-29 13:05:40 +0200

    React Startup, Fetching Data

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+
 
 function App() {
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() =>{
+    axios.get('http://localhost:5000/api/jobs').then(response =>{
+      setJobs(response.data);
+    })
+  }, [])
+
+
   return (
     <div className="App">
+     
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          <ul>
+            {jobs.map((job: any) => (
+              <li key={job.id}>
+                {job.title}
+              </li>
+            ))}
+          </ul>
+        
       </header>
     </div>
   );
